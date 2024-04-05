7151096d09297355f8ede4fcb48454ced01410e6
MattiaMessana <mattia.messana@icloud.com>
2024-04-05 11:45:36 +0200

    add:snak1

---
+++ jsSnak1/js/functions.js
@@ -0,0 +1,18 @@
+// Fai inserire un numero, che chiameremo N, all’utente.
+// Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+// Ogni volta che ne crei uno, stampalo a schermo.
+
+//1. tramite pormpt chiediamo un numero all'utente 
+const N = parseInt(prompt("dimmi un numero"));
+console.log(N);
+
+//2. creiamo una funzione che generi n arrey (n = numero utente)
+function generateArrey () {
+
+    
+}
+
+//3. creiamo una funzione che in ogni arrey inserisca 10 numeri casuali tra 1 e 100
+
+
+//4. creiamo un ciclo for che ogni volta che viene generato un arrey lo stampi in pagina
